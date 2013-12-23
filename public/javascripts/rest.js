95e26aa6dcc01901422aee6da23d9fc794876b56
Alex Egan <aegan@andrew.cmu.edu>
2013-12-23 13:25:42 -0500

    can put

--- public/javascripts/rest.js
+++ public/javascripts/rest.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-	function newMsgSubmit() {
-		alert("hi");
-	}
-
-	$("#new-msg-form").submit(newMsgSubmit);
-
+	$("#submit").click(function(e) {
+		e.preventDefault();
+		var auth = $("#author").val();
+		var rex = new RegExp(find, 'g');
+		auth = auth.replace(rex, "%20");
+		var msg = $("#message").val();
+		msg = msg.replace(rex, "%20");
+		$("#author").val("");
+		$("#message").val("");
+		var urlStr = "/put?author=" + auth + "&message=" + msg;
+		$.ajax({
+			url: urlStr,
+			type: "put",
+			success: function(data) {
+			}
+		});
+	});
 });
+
+function newMsgFunction() {
+
+}
